774bbb028039eb98797727ba235675a8664e1073
mai <58199372+maiqvu@users.noreply.github.com>
2020-02-04 15:30:42 +1100

    enabled actioncable authentication with jwt

--- app/jobs/chat_message_creation_event_broadcast_job.rb
+++ app/jobs/chat_message_creation_event_broadcast_job.rb
@@ -2,8 +2,8 @@ class ChatMessageCreationEventBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform( chat_message )
-    puts '================================='
-    p chat_message
+    # puts '============================='
+    # p chat_message
     ActionCable
       .server
       .broadcast('chat_channel',
