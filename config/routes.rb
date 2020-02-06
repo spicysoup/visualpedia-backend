a8e184b661f8c91127dca081bf68ec168ff7307b
Harry Patrick <harry.g.patirck94@gmail.com>
2020-02-06 16:30:50 +1100

    commit to pull

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,7 @@ Rails.application.routes.draw do
   get '/indicators/:label' => 'indicators#show'
 
   mount ActionCable.server => '/cable'
-<<<<<<< HEAD
-=======
-  
   get 'indicators' => 'indicators#index'
   get 'indicators/:label' => 'indicators#show'
->>>>>>> edfc96327b35e59603192f8b8b8babc48d24c928
+
 end
